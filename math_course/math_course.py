43a103904eaa14aab250b8045e93a350be40c30a
Danila <itsoke@mail.ru>
2024-06-08 03:50:31 +0300

    Cleared code

--- math_course/math_course.py
+++ math_course/math_course.py
@@ -5,13 +5,6 @@ from util.load_files import load_files
 from util.subject_dialogue import SubjectDialogue
 
 
-# math_data_path = os.path.join(PROJECT_ROOT_PATH, 'math_course', 'data')
-# def send_math_options(message, bot):
-#     bot.send_message(message.chat.id, MATH_TRAINING_MATERIALS)
-#     media = load_files(math_data_path)
-#     bot.send_media_group(message.chat.id, media)
-
-
 class MathSubjectDialogue(SubjectDialogue):
     def __init__(self, bot):
         materials_path = os.path.join(PROJECT_ROOT_PATH, 'math_course', 'data')
