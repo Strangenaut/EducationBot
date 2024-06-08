3f4cb86632ecda959e9bf58ccb0798d123f50066
strangenaut <denisgullu917@gmail.com>
2024-06-08 04:22:57 +0300

    Changed buttons style

--- math_course/math_course.py
+++ math_course/math_course.py
@@ -1,4 +1,5 @@
 import os
+from telebot import types
 from config import *
 from math_course.config import *
 from util.load_files import load_files
@@ -9,11 +10,11 @@ class MathSubjectDialogue(SubjectDialogue):
     def __init__(self, bot):
         materials_path = os.path.join(PROJECT_ROOT_PATH, 'math_course', 'data')
         options_descriptions = [
-            DIFF_EQUATIONS,
+            DIFF_EQUATIONS, 
             LINAL,
-            MATH_AN,
+            MATH_AN, 
             PREVIOUS_TASKS_EXAMPLES,
-            PROB_THEORY,
+            PROB_THEORY, 
             STATS,
             BACK
         ]
@@ -37,6 +38,25 @@ class MathSubjectDialogue(SubjectDialogue):
             return
         
         self.send_training_materials(message)
+
+    def send_options(self, message):
+        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+
+        i = 1
+        while i < len(self.options_descriptions):
+            markup.row(
+                self.options_descriptions[i - 1], 
+                self.options_descriptions[i]
+            )
+            i += 2
+        markup.add(self.options_descriptions[-1])
+
+        self.bot.send_message(
+            message.chat.id, 
+            text=MAKE_CHOICE, 
+            parse_mode='html', 
+            reply_markup=markup
+        )
 
     def send_training_materials(self, message):
         subject_name = self.translation_dict[message.text]
